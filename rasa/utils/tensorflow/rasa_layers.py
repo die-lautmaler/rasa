5f7e60969a8a0bfb29a1660bfc38d72b410ecc2a
fbublitz <frank.bublitz@die-lautmaler.de>
2025-01-09 10:15:42 +0100

    🚧 adaptions

--- rasa/utils/tensorflow/rasa_layers.py
+++ rasa/utils/tensorflow/rasa_layers.py
@@ -307,7 +307,7 @@ class ConcatenateSparseDenseFeatures(RasaCustomLayer):
         if self.SPARSE_DROPOUT in self._tf_layers:
             feature = self._tf_layers[self.SPARSE_DROPOUT](feature, training=training)
 
-        feature = self._tf_layers[self.SPARSE_TO_DENSE](feature)
+        feature = self._tf_layers[self.SPARSE_TO_DENSE](feature, training=training)
 
         if self.DENSE_DROPOUT in self._tf_layers:
             feature = self._tf_layers[self.DENSE_DROPOUT](feature, training=training)
